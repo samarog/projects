f6bf1166937016de62aa1b2263712fd02953b718
Gonçalo Amaro <samaro.dev@gmail.com>
2025-08-17 00:23:34 +0100

    Restructure project file tree

---
+++ 11.1 Dice City/index.js
@@ -0,0 +1,47 @@
+function rollDice() {
+  const randomNumber1 = Math.floor(Math.random()*6) + 1
+  console.log(`Player 1: ${randomNumber1}`)
+  
+  if (randomNumber1 === 1) {
+    document.querySelector('.img1').setAttribute('src', '.assets/images/diceS1.png')
+  } else if (randomNumber1 === 2) {
+    document.querySelector('.img1').setAttribute('src', '.assets/images/diceS2.png')
+  } else if (randomNumber1 === 3) {
+    document.querySelector('.img1').setAttribute('src', '.assets/images/diceS3.png')
+  } else if (randomNumber1 === 4) {
+    document.querySelector('.img1').setAttribute('src', '.assets/images/diceS4.png')
+  } else if (randomNumber1 === 5) {
+    document.querySelector('.img1').setAttribute('src', '.assets/images/diceS5.png')
+  } else {
+    document.querySelector('.img1').setAttribute('src', '.assets/images/diceS6.png')
+  }
+  
+  const randomNumber2 = Math.floor(Math.random()*6) + 1
+   console.log(`Player 2: ${randomNumber2}`)
+  
+  if (randomNumber2 === 1) {
+    document.querySelector('.img2').setAttribute('src', '.assets/images/diceS1.png')
+  } else if (randomNumber2 === 2) {
+    document.querySelector('.img2').setAttribute('src', '.assets/images/diceS2.png')
+  } else if (randomNumber2 === 3) {
+    document.querySelector('.img2').setAttribute('src', '.assets/images/diceS3.png')
+  } else if (randomNumber2 === 4) {
+    document.querySelector('.img2').setAttribute('src', '.assets/images/diceS4.png')
+  } else if (randomNumber2 === 5) {
+    document.querySelector('.img2').setAttribute('src', '.assets/images/diceS5.png')
+  } else {
+    document.querySelector('.img2').setAttribute('src', '.assets/images/diceS6.png')
+  }
+
+  if (randomNumber1 === randomNumber2) {
+    document.querySelector('h2').innerHTML = "It's a tie...";
+  } else if (randomNumber1 > randomNumber2) {
+    document.querySelector('h2').innerHTML = "Player 1 wins!";
+  } else if (randomNumber1 < randomNumber2) {
+    document.querySelector('h2').innerHTML = "Player 2 wins!";
+  }
+}
+
+
+
+
--- README.md
+++ README.md
@@ -5,7 +5,8 @@
 | Project Name | Link | Description |
 |--------------|------|-------------|
 | **Janken** | [Open](https://samarog.github.io/projects/14.1%20Janken/game.html) | A Japanese classical game |
-| **Dice City** | [Open](https://samarog.github.io/projects/17.1%20Dice%20City/dicecity.html) | A simple dice-throwing game |
+| **Dice City** | [Open](https://samarog.github.io/projects/11.1%20Dice%20City/dicecity.html) | A simple dice-throwing game |
+| **Predator Countdown** | [Open](https://samarog.github.io/projects/17.1%20Predator%20Countdown/index.html) | A study on CSS 3D manipulation |
 | **dooMPC** | [Open](https://samarog.github.io/projects/18.1%20MPC%20Sim/index.html) | A 16-pad MPC simulator |
 | **Calc** | [Open](https://samarog.github.io/projects/18.2%20Calculator/index.html) | A calculator (pretty much self-explanatory) |
 | **Hello Worlds!** | [Open](https://samarog.github.io/projects/19.1%20Hello%20Worlds!/index.html) | A project built entirely in jQuery |
